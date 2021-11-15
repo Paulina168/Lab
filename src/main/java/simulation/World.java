550c70547dac58b98490c2c85df026346901bb8d
Paulina <pgierlak2000@gmail.com>
2021-11-15 17:03:59 +0100

    After lab6

--- src/main/java/simulation/World.java
+++ src/main/java/simulation/World.java
@@ -5,22 +5,64 @@ import java.util.List;
 import java.util.Random;
 
 public class World {
-
-    public static final Random random = new Random();
-    private static final Animal animal = new Animal(new Vector2D(random.nextInt(100), random.nextInt(100)));
+    private static final int DAYS_NO = 10;
+    private static final Random random = new Random();
 
     public static void main(String[] args) {
         System.out.println("Start");
-        run();
+        for (int i = 0; i < DAYS_NO; i++) {
+            Simulation.simulateDay();
+        }
         System.out.println("Stop");
     }
 
-    private static void run () {
-       for(int i = 0; i < 10; i++) {
-           animal.move(MapDirection.values()[random.nextInt(MapDirection.values().length)]);
-       }
+    private static void moveAnimal(Animal animal) {
+        for (int i = 0; i < 10; i++) {
+            animal.move(MapDirection.values()[random.nextInt(MapDirection.values().length)]);
+        }
     }
 
+    private static void run(String[] args) {
+        for (String arg : args) {
+            switch (arg) {
+                case "n" -> System.out.println("Animal moves north");
+                case "e" -> System.out.println("Animal moves east");
+                case "s" -> System.out.println("Animal moves south");
+                case "w" -> System.out.println("Animal moves west");
+                default -> System.out.println("Wrong argument");
+            }
+        }
+    }
 
+    private static void runFromDirections(List<MapDirection> directions) {
+        for (MapDirection direction : directions) {
+            switch (direction) {
+                case NORTH -> System.out.println("Animal moves north");
+                case EAST -> System.out.println("Animal moves east");
+                case SOUTH -> System.out.println("Animal moves south");
+                case WEST -> System.out.println("Animal moves west");
+                default -> System.out.println("Wrong argument");
+            }
+        }
+    }
 
+    private static void runFromDirections2(List<MapDirection> directions) {
+        for (MapDirection direction : directions) {
+            System.out.println("Animal moves " + direction);
+        }
+    }
+
+    private static List<MapDirection> parseToEnum(String[] args) {
+        List<MapDirection> directions = new ArrayList<>();
+        for (String arg : args) {
+            switch (arg) {
+                case "n" -> directions.add(MapDirection.NORTH);
+                case "e" -> directions.add(MapDirection.EAST);
+                case "s" -> directions.add(MapDirection.SOUTH);
+                case "w" -> directions.add(MapDirection.WEST);
+                default -> System.out.println("Wrong argument: " + arg);
+            }
+        }
+        return directions;
+    }
 }
